9a3d9318553c272a1ecf7e8ffbff1e525bc3896b
Blake Georgii <blakegeorgii@gmail.com>
2022-01-31 11:51:00 -0800

    created get for all posts

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRouters = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRouters);
+router.use('/post', postRoutes);
 
 module.exports = router;
