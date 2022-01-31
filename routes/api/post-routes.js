06b1dfe699c2b64cedc17759619d45b17353e294
Blake Georgii <blakegeorgii@gmail.com>
2022-01-31 12:17:05 -0800

    reordered get route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -4,6 +4,7 @@ const { Post, User } = require('../../models');
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [{ model: User, attributes: ['username'] }]
     })
         .then(dbPostData => { res.json(dbPostData) })
