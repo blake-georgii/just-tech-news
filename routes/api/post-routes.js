d12575896e748c9c42f9c116ca6299890edfbfe9
Blake Georgii <blakegeorgii@gmail.com>
2022-02-01 22:10:59 -0800

    implementing comments into other routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
 
-// get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
