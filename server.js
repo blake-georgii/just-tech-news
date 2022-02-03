fbd816e09710a490be5fa5f96bfc37d525b8d6c7
Blake Georgii <blakegeorgii@gmail.com>
2022-02-02 18:54:33 -0800

    added handlebars

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
