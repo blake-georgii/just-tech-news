e2feb6caa06ba140f25c95c425401ccd1e738018
Blake Georgii <blakegeorgii@gmail.com>
2022-02-07 11:40:03 -0800

    added login page route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,12 +29,16 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       // pass a single post object into the homepage template
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', {posts});
+      res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
