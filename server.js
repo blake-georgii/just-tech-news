f0d5846654002b2d984cca360c08092abecf39ca
Blake Georgii <blakegeorgii@gmail.com>
2022-02-10 12:13:21 -0800

    imlpemented helper functions

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./controllers/');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
