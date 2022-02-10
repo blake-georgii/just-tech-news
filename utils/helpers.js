3f5c3f7efdf9e7ee23791760b30e5f3c2a3029ed
Blake Georgii <blakegeorgii@gmail.com>
2022-02-10 12:08:18 -0800

    added util to format urls

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,9 +5,17 @@ module.exports = {
         ).getFullYear()}`;
     },
     format_plural: (word, amount) => {
-        if(amount !== 1){
+        if (amount !== 1) {
             return `${word}s`;
         }
         return word;
+    },
+    format_url: url => {
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
     }
 }
